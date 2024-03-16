83c59309ddf3e0c6ff40fcbd34fe7dace7572377
NewComer12616561 <legiakhanh2015@gmail.com>
2024-03-17 00:39:13 +0700

    fix cant resolve module

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
-import Header from "@/components/layout/Header";
-import Hero from "@/components/layout/Hero";
-import HomeMenu from "@/components/layout/HomeMenu";
-import SectionHeader from "@/components/layout/SectionHeaders";
+
+import Hero from "../components/layout/Hero";
+import HomeMenu from "../components/layout/HomeMenu";
+import SectionHeader from "../components/layout/SectionHeaders";
 
 
 
