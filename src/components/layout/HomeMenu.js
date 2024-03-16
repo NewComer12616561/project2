5603c7e510405d1f023bc7eaae2e90f25c36f414
NewComer12616561 <legiakhanh2015@gmail.com>
2024-03-16 18:44:07 +0700

    Added my project

---
+++ src/components/layout/HomeMenu.js
@@ -0,0 +1,44 @@
+import Image from "next/image";
+import { MenuItem } from "../menu/MenuItem";
+import SectionHeader from "./SectionHeaders";
+
+
+export default function HomeMenu(){
+    return(
+    
+<section className="">
+    <div className="absolute  left-0 right-0 w-full
+    justif-start ">
+        <div className=" absolute -left-0 -top-[70px] text-left -z-10 ">
+        <Image src={'/sallad1.png'} width={'109'} height={'189'} alt={'sallad'} />
+        </div>
+
+        <div className=" absolute -top-[100px] -right-0 -z-10">
+        <Image src={'/sallad2.png'} width={'107'} height={'195'} alt={'sallad'} />
+
+        </div>
+       
+    </div>
+        
+        <div className="text-center mb-4">
+      <SectionHeader
+      subHeader={'check out'}
+      mainHeader={'Menu'}/>
+        </div>
+        
+        <div className="grid grid-cols-3 gap-4">
+           <MenuItem/>
+           <MenuItem/>
+           <MenuItem/>
+           <MenuItem/>
+           <MenuItem/>
+           <MenuItem/>
+        </div>
+        
+       
+
+
+</section> 
+    );
+
+}
---
+++ src/components/menu/MenuItem.js
@@ -0,0 +1,18 @@
+export function MenuItem(){
+    return(
+        <div className="bg-gray-200 p-4 rounded-lg text-center
+         group hover:bg-white hover:shadow-md hover:shadow-black/25 transition-all">
+        <div className="text-center ">    
+        <img src="/pizza.png" className="max-h-auto max-h-24 block mx-auto"
+         alt="pizza"/>
+        </div>
+        <h4 className="font-semibold text-xl my-2 "> Pepperoni Pizza</h4>
+        <p className="text-gray-500 text-sm">
+            text text text text MenuItem.js
+        </p>
+        <button className="mt-4 bg-primary text-white rounded-full
+        px-6 py-2">Add to cart for $12 </button>
+    </div>
+
+    );
+}
