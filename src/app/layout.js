aecfa969d7f6c8e65b3b60c38ed0f80e1fe5439b
NewComer12616561 <legiakhanh2015@gmail.com>
2024-03-25 21:08:02 +0700

    Hit 2nd mark

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Roboto } from "next/font/google";
 import "./globals.css";
 import Header from "../components/layout/Header";
 import {AppProvider} from "@/components/AppContext";
+import { Toaster } from 'react-hot-toast';
 
 
 const roboto = Roboto({ subsets: ["latin"], weight:['400', '500','700'] });
@@ -17,6 +18,7 @@ export default function RootLayout({ children }) {
       <body className={roboto.className}>
         <main className="max-w-4xl mx-auto  p-4">
       <AppProvider>
+        <Toaster/>
       <Header />
 
         {children}
--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -4,54 +4,95 @@ import { useSession } from "next-auth/react";
 import Image from 'next/image';
 import { redirect } from "next/navigation";
 import { useEffect, useState } from "react";
-import InfoBox from "../../components/layout/InfoBox";
-import SuccessBox from "../../components/layout/SuccessBox";
+
+import toast from "react-hot-toast";
 
 export default function ProfilePage(){
     const session = useSession();
+   
     const[userName, setUserName] =useState( '');
     const[image, setImage] =useState('');
-    const [saved, setSaved] = useState(false);
-    const [isSaving, setIsSaving] = useState(false);
-    const [isUploading, setIsUploading] = useState(false);
+    const[phone, setPhone] =useState('');
+    const [streetAddress, setStreetAddress] = useState('');
+    
+    const[postalCode, setPostalCode] =useState('');
+    const[city, setCity] =useState('');
+    const[country, setCountry] =useState('');
     const {status}= session;
     
     useEffect(()=>{
         if(status==='authenticated'){
             setUserName(session.data.user.name);
             setImage(session.data.user.image);
+            fetch('/api/profile').then(response =>{
+                response.json().then(data =>{
+                    setPhone(data.phone);
+                  //something went wrong with street address
+                    setStreetAddress(data.streetAddress);
+                    setPostalCode(data.postalCode);
+                    setCity(data.city);
+                    setCountry(data.country);
+                    
+                });
+            })
         }
     }, [session ,status] );
 
     async function handleProfileInfoUpdate(ev){
         ev.preventDefault();
-        setSaved(false);
-        setIsSaving(true);
-        const response = await fetch('/api/profile/', {
-            method:'PUT',
-            headers:{'Content-Type':'application/json'},
-            body: JSON.stringify({name:userName, image}),
+        
+        const savingPromise = new Promise(async (resolve, reject) => {
+            const response = await fetch('/api/profile/', {
+                method:'PUT',
+                headers:{'Content-Type':'application/json'},
+                body: JSON.stringify({
+                    name:userName, 
+                    image,
+                    streetAddress,
+                    phone,
+                    postalCode,
+                    city,
+                    country
+                }),
         });
-        setIsSaving(false);
-        if(response.ok){
-            setSaved(true);
-        }
-
+        if(response.ok) 
+         resolve()
+        else 
+         reject();
+        });
+        toast.promise(
+            
+            savingPromise, {
+            loading:'Saving...',
+            success:'Profile saved',
+            error:'Error',
+        });
     }
     //Primarly Picture
     async function handleFileChange(ev){
        const files = ev.target.files;
        if(files?.length === 1 ){
         const data = new FormData;
-        data.set('file', files[0])
-        setIsUploading(true);
-        const response = await fetch('/api/upload',{
-            method:'POST',
-            body: data,
-        });        
-        const link = await response.json();
-        setImage(link);
-        setIsUploading(false);
+        data.set('file', files[0]);
+
+        const uploadPromise =  fetch('/api/upload',{
+                method:'POST',
+                body: data,
+            }).then (response=> {
+                if (response.ok) {
+                    return response.json().then(link => {
+                    setImage(link);
+                    })
+                }
+                throw new Error('Something went wrong');
+            });            
+        
+       await toast.promise(uploadPromise,{ 
+        loading:'Uploading...',
+        success:'Upload saved',
+        error:'Upload Error',
+    });       
+        
        }
     }
     
@@ -64,23 +105,12 @@ export default function ProfilePage(){
         return redirect('/login');
     }
 
-   
-
     return(
         <section className="mt-8">
-            <h1 className="text-center text-primary mb-4">Profile</h1>
+            <h1 className="text-center text-primary text-4xl mb-4">Profile</h1>
            
         <div className="max-w-md mx-auto ">
-            {saved &&(
-               <SuccessBox>Profile updated</SuccessBox>
-            )}
-            {isSaving&& (
-                <InfoBox>Updating</InfoBox>
-             )}
-             {isUploading &&(
-                 <InfoBox>Updating image...</InfoBox>
-             )}
-            <div className="flex gap-4 items-center">
+            <div className="flex gap-4 ">
                 <div>
                    
                 <div className=" p-2 rounded-lg relative max-w-[120px]">
@@ -99,9 +129,35 @@ export default function ProfilePage(){
                 </div>
                 </div>
                 <form className="grow" onSubmit={handleProfileInfoUpdate}>
+                    <label>First and last name</label>
                 <input type="text" placeholder="Input First and Last name"
                 value={userName} onChange={ev => setUserName(ev.target.value)}/>
-                <input type="email" disabled={true} value={session.data.user.email}/>
+                    <label>Email</label>
+                <input type="email" disabled={true} value={session.data.user.email}
+                placeholder={'email'}/>
+                   <label>Phone</label>
+                <input type ="tel" placeholder="Phone number"
+                value={phone} onChange={ev => setPhone(ev.target.value)} />
+                    <label>Street address</label>
+                <input type ="text" placeholder="Street address"
+                value={streetAddress} onChange={ev => setStreetAddress(ev.target.value)} />
+                
+                <div className="flex gap-2">
+                    <div>
+                    <label>Postal code</label>
+                    <input type ="text" placeholder="Postal code"
+                    value={postalCode} onChange={ev => setPostalCode(ev.target.value)} /> 
+                    </div>
+                    <div>
+                    <label>City</label>
+                    <input  type ="text" placeholder="City"
+                    value={city} onChange={ev => setCity(ev.target.value)}/>
+                    </div>
+                </div>
+                <label>Country</label>
+                <input type ="text" placeholder="Country"
+                value={country} onChange={ev => setCountry(ev.target.value)}/>
+
                 <button type ="submit">Save</button> 
                 </form>
                 
